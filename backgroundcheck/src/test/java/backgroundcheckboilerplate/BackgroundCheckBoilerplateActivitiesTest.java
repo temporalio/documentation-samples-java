2ed0f587f6f77c10e16b7ad905d16dbc32edaf48
Mason Egger <mason@masonegger.com>
2023-10-19 15:35:49 -0500

    adding more content around Activity Testing

--- backgroundcheck/src/test/java/backgroundcheckboilerplate/BackgroundCheckBoilerplateActivitiesTest.java
+++ backgroundcheck/src/test/java/backgroundcheckboilerplate/BackgroundCheckBoilerplateActivitiesTest.java
@@ -41,12 +41,25 @@ public class BackgroundCheckBoilerplateActivitiesTest {
 
 }
 
+/*
+Temporal provides the `TestWorkflowExtension` class to simplify the creation of 
+the test environment. Using this extension you provide your Activity 
+to register with a Worker created by the testing framework to be used during testing. 
+The extension provides a `TestWorkflowEnvironment`, `Worker`, and a stubbed Activity 
+object to each test as well as manage the lifecycle of the test environment.
+If you require more granular control of the test environments, you can manually create
+and destroy all these parts in methods annotated with `@BeforeEach` and `@AfterEach`
+as defined by JUnit.
+
+You annotate the method with @Test and test the results of the Activity via assertions.
+*/
+
 /* @dacx
-id: backgroundcheck-boilerplate-add-test-framework-activities
+id: backgroundcheck-boilerplate-add-activity-tests
 title: Testing Activities
 description: How to add a testing framework to your Temporal Application.
 label: Test framework
-lines: 4-28
+lines: 4-42
 tags:
 - testing
 - developer guide
@@ -55,11 +68,11 @@ tags:
 @dacx */
 
 /* @dacx
-id: backgroundcheck-boilerplate-add-activity-tests
+id: backgroundcheck-boilerplate-add-activity-tests-details
 title: Add Activity method tests
 description: How to test Activity code
 label: Test Activity code
-lines: 21-42
+lines: 44-55
 tags:
 - testing
 - developer guide
