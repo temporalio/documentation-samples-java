2d2219ae352204f7604253c235bb13723ccffac6
Mason Egger <mason@masonegger.com>
2023-10-19 16:03:21 -0500

    updating Workflow Test docs

--- backgroundcheck/src/test/java/backgroundcheckboilerplate/BackgroundCheckBoilerplateWorkflowTest.java
+++ backgroundcheck/src/test/java/backgroundcheckboilerplate/BackgroundCheckBoilerplateWorkflowTest.java
@@ -12,30 +12,28 @@ import io.temporal.testing.TestWorkflowExtension;
 import io.temporal.worker.Worker;
 
 /*
-**How to add a Testing Framework and Tests for Workflows.**
+Temporal provides the `TestWorkflowEnvironment` and `TestWorkflowExtension` classes
+to allow for testing Workflows. There are two ways to test Workflows; the first
+is to test the Workflow code without invoking the real Activities by mocking
+the Workflow's Activities and the second is to test the Workflow and its Activities
+in their entirety. This section will focus on the first scenario while a following.
+section will cover the later. 
 
-We can test Workflow code for the following conditions:
-- Workflow status. For example, did the Workflow reach a completed status?
-- Workflow threw an exception. Did the Workflow function return an error?
-- Error when checking for a result of a Workflow. Is there an error in getting the result returned by the Workflow?
-- Workflow return value. If the Workflow did return something other than an error, is it what you expected it to be?
+Testing Workflows is similar to testing non-Temporal java code.
+
+Some examples of things an Workflow can be tested for are:
+- Exceptions thrown when invoking the Workflow Execution.
+- Exceptions thrown when checking for the result of the Workflow Execution.
+- Workflow return values. Check to ensure the return value is expected.
 
 We can also perform a Workflow Replay test, and we'll provide detailed coverage of this topic in another section.
 */
 
-/* To test Workflow code in isolation, that is, without invoking the Activity
-   code and only testing the Workflow code, use Mocking. 
-
-   This example demonstrates how to use Mockito to mock an Activity and have
-   your Workflow invoke the mocked Activity.
- */
-
 public class BackgroundCheckBoilerplateWorkflowTest {
 
-  /* Use JUnit Extensions to simplify the creation of the test environment.
-     This creates an environment and registers the Workflow to a Worker for testing.
-     If you would rather set this up yourself, look into TestWorkflowEnvironment
-  */
+  // Use JUnit Extensions to simplify the creation of the test environment.
+  // This creates an environment and registers the Workflow to a Worker for testing.
+  // If you would rather set this up yourself, look into TestWorkflowEnvironment
   @RegisterExtension
   public static final TestWorkflowExtension testWorkflowExtension = TestWorkflowExtension
       .newBuilder().setWorkflowTypes(BackgroundCheckBoilerplateWorkflowImpl.class)
@@ -49,8 +47,8 @@ public class BackgroundCheckBoilerplateWorkflowTest {
     BackgroundCheckBoilerplateActivities mockedActivities =
         mock(BackgroundCheckBoilerplateActivities.class, withSettings().withoutAnnotations());
 
-    /* Specify what value should be returned when a specific Activity is invoked.
-       Your Activity must have the same method name here as it would within your Workflow */
+    // Specify what value should be returned when a specific Activity is invoked.
+    // Your Activity must have the same method name here as it would within your Workflow
     when(mockedActivities.ssnTraceActivity("Angela")).thenReturn("pass");
 
     // Register the Workflow's Activities with the Worker provided by the Extension
@@ -59,11 +57,9 @@ public class BackgroundCheckBoilerplateWorkflowTest {
     // Start the test environment
     testEnv.start();
 
-    /* Request execution of the backgroundCheck Workflow
-       This will execute your Workflow, calling the Mocked Activities in place
-       of your actual implementation of the Activities.
-    */
-
+    // Request execution of the backgroundCheck Workflow
+    // This will execute your Workflow, calling the Mocked Activities in place
+    // of your actual implementation of the Activities.
     String pass_output = workflow.backgroundCheck("Angela");
   
     assertEquals("pass", pass_output);
@@ -71,12 +67,20 @@ public class BackgroundCheckBoilerplateWorkflowTest {
   }
 }
 
+/* 
+To test Workflow code in isolation, that is, without invoking the Activity
+code and only testing the Workflow code, use Mocking. 
+
+This example demonstrates how to use Mockito to mock an Activity and have
+your Workflow invoke the mocked Activity.
+*/
+
 /* @dacx
-id: backgroundcheck-boilerplate-add-test-framework-workflow
-title: Add a testing framework for Workflows
-description: How to add a testing framework to your Temporal Application for your Workflows
+id: backgroundcheck-boilerplate-add-workflow-tests
+title: Testing Workflows
+description: How to test your Temporal Application Workflows
 label: Test framework details
-lines: 4-24, 33-42
+lines: 4-70
 tags:
 - testing
 - developer guide
@@ -85,7 +89,7 @@ tags:
 @dacx */
 
 /* @dacx
-id: backgroundcheck-boilerplate-add-workflow-test-mock
+id: backgroundcheck-boilerplate-add-workflow-tests-details
 title: Add Workflow method test using Mocks
 description: Details about how to test Workflow code in isolation using Mocks
 label: Test Workflow code details
@@ -93,5 +97,5 @@ tags:
 - testing
 - developer guide
 - java sdk
-lines: 26-72
+lines: 72-78
 @dacx */
