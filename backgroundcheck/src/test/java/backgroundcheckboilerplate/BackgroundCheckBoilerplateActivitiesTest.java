6d02df6e404c16b524ec0df57ce9cf5752dfa93b
Mason Egger <mason@masonegger.com>
2023-10-24 09:51:17 -0500

    fixing copy paste error in comments

--- backgroundcheck/src/test/java/backgroundcheckboilerplate/BackgroundCheckBoilerplateActivitiesTest.java
+++ backgroundcheck/src/test/java/backgroundcheckboilerplate/BackgroundCheckBoilerplateActivitiesTest.java
@@ -44,11 +44,11 @@ public class BackgroundCheckBoilerplateActivitiesTest {
 }
 
 /*
-Temporal provides the `TestWorkflowExtension` class to simplify the creation of 
+Temporal provides the `TestActivityExtension` class to simplify the creation of 
 the test environment. Using this extension you provide your Activity 
 to register with a Worker created by the testing framework to be used during testing. 
-The extension provides a `TestWorkflowEnvironment`, `Worker`, and a stubbed Activity 
-object to each test as well as manage the lifecycle of the test environment.
+The extension provides a a stubbed Activity  object to each test as well as 
+manage the lifecycle of the test environment.
 If you require more granular control of the test environments, you can manually create
 and destroy all these parts in methods annotated with `@BeforeEach` and `@AfterEach`
 as defined by JUnit.
